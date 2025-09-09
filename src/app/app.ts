328f3baefac6c5b2845a1779beb5c514c8f4ab2e
Ebrahim Salehipanah <e.salehipanah@gmail.com>
2025-09-09 23:52:18 +0330

    add more info in demo page

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,11 +13,40 @@ export class App {
   isLoading = true;
 
   cards = [
-    { id: 1,title: 'Dashboard Card 1', text: 'This card shows content once loading is finished.' },
-    { id: 2,title: 'Dashboard Card 2', text: 'Skeleton placeholders improve user experience.' },
-    { id: 3,title: 'Dashboard Card 3', text: 'You can customize width, height, and border radius.' },
+    { id: 1, title: 'Dashboard Card 1', text: 'This card shows content once loading is finished.' },
+    { id: 2, title: 'Dashboard Card 2', text: 'Skeleton placeholders improve user experience.' },
+    {
+      id: 3,
+      title: 'Dashboard Card 3',
+      text: 'You can customize width, height, and border radius.',
+    },
+    { id: 4, title: 'Dashboard Card 1', text: 'This card shows content once loading is finished.' },
+    { id: 5, title: 'Dashboard Card 2', text: 'Skeleton placeholders improve user experience.' },
+    {
+      id: 6,
+      title: 'Dashboard Card 3',
+      text: 'You can customize width, height, and border radius.',
+    },
+    { id: 7, title: 'Dashboard Card 1', text: 'This card shows content once loading is finished.' },
+    { id: 8, title: 'Dashboard Card 2', text: 'Skeleton placeholders improve user experience.' },
+    {
+      id: 9,
+      title: 'Dashboard Card 3',
+      text: 'You can customize width, height, and border radius.',
+    },
+    {
+      id: 10,
+      title: 'Dashboard Card 1',
+      text: 'This card shows content once loading is finished.',
+    },
+    { id: 11, title: 'Dashboard Card 2', text: 'Skeleton placeholders improve user experience.' },
+    {
+      id: 12,
+      title: 'Dashboard Card 3',
+      text: 'You can customize width, height, and border radius.',
+    },
   ];
-toggleLoading(){
-  this.isLoading = !this.isLoading;
-}
+  toggleLoading() {
+    this.isLoading = !this.isLoading;
+  }
 }
